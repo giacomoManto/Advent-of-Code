45ae2979651c90d5f20e965af6538937ab618197
Giacomo Mantovanelli <104456506+giacomoManto@users.noreply.github.com>
2024-12-04 18:44:46 -0500

    Day 2

---
+++ Day2/D2P1.py
@@ -0,0 +1,29 @@
+inputfile = "Day2\input.txt"
+
+safe = 0
+
+with open(inputfile, "r") as input:
+    for line in input.readlines():
+        line = line.removesuffix("\n").split(" ")
+        line = [int(a) for a in line]
+        increasing = None
+        safeLine = True
+        for level in range(len(line)):
+            if level == 0:
+                increasing = line[level + 1] > line[level]
+                continue
+            if increasing:
+                if line[level - 1] >= line[level]:
+                    safeLine = False
+                    break
+            else:
+                if line[level - 1] <= line[level]:
+                    safeLine = False
+                    break
+            if abs(line[level - 1] - line[level]) > 3:
+                safeLine = False
+                break
+        if safeLine:
+            safe += 1
+
+print(safe)
---
+++ Day2/D2P2.py
@@ -0,0 +1,40 @@
+inputfile = "Day2\input.txt"
+
+
+def safetyCheck(line: list) -> int:
+    increasing = None
+    for index in range(len(line)):
+        if index == 0:
+            increasing = line[index + 1] > line[index]
+            continue
+        if increasing:
+            if line[index - 1] >= line[index]:
+                return index
+        else:
+            if line[index - 1] <= line[index]:
+                return index
+        if abs(line[index - 1] - line[index]) > 3:
+            return index
+    return -1
+
+
+safe = 0
+
+with open(inputfile, "r") as input:
+    for line in input.readlines():
+        line = line.removesuffix("\n").split(" ")
+        line = [int(a) for a in line]
+        fail = safetyCheck(line)
+        if fail != -1:
+            l0 = line.copy()
+            l0.pop(0)
+            l1 = line.copy()
+            l1.pop(fail)
+            l2 = line.copy()
+            l2.pop(fail - 1)
+            if safetyCheck(l1) == -1 or safetyCheck(l2) == -1 or safetyCheck(l0) == -1:
+                safe += 1
+        else:
+            safe += 1
+
+print(safe)
