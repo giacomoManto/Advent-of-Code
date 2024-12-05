a2afd782849e76af78aea2bbdd77647f69656e5f
Giacomo Mantovanelli <gmantovanelli@gentuity.com>
2024-12-05 13:29:20 -0500

    2015/Day1

--- 2015/Day1/D1.py
+++ 2015/Day1/D1.py
@@ -1,4 +1,20 @@
 inputfile = "2015/Day1/input.txt"
 
 with open(inputfile, "r") as input:
-    pass
+    data = input.read().removesuffix("\n")
+
+
+runningTally = 0
+pos = 1
+for char in data:
+    if char == "(":
+        runningTally += 1
+    elif char == ")":
+        runningTally -= 1
+    if runningTally < 0:
+        print(pos)
+        break
+    pos += 1
+
+print(f"Part one: {data.count("(") - data.count(")")}")
+print(f"Part two: {pos}")
